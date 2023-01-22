4886fbe27f38c79633e3a0bbcb48e8f8c1664474
Lakmal Asela <53096736+lakamalasela@users.noreply.github.com>
2023-01-22 15:03:28 +0530

    expense_tracker

---
+++ src/middleware/checkAuth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async(req,res, next)=>{
+    try{
+        const token = req.headers.authorization.split(' ')[1];//space eken Bearer kiyna eka seperate kra gnnwa ' ' white space eken wen kra gnne 0,1 kiyla
+        const decoded = jwt.verify(token,"MY_TOP_SECRET_KEY");//ape token ekai secrete key ekai compaire krla verify kra gnnwa 
+        req.user = decoded;
+        next(); //next kiynne ape api ekata ynna kiyla
+        const {userId} = req.user; //user kawda kiyla attched krnwa
+    }catch(error){
+        const details = JSON.parse(JSON.stringify(error));
+        if(details.message){
+            return res.status(401).json(details.message);
+        }
+        return res.sendStatus(401);
+    }
+}
